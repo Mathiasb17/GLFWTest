2ae9d497512fd4e6d45feff96c99be0d1472055a
Mathiasb17 <mathiasb17@gmail.com>
2016-11-29 12:07:51 +0100

    some helper functions

--- main2.cpp
+++ main2.cpp
@@ -10,19 +10,38 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
-
 /**********************************************************************
- *                       SOME HELPER FUNCTIONS                        *
+ *                WINDOW AND MOUSE/KEYBOARD PARAMETERS                *
  **********************************************************************/
 
+GLFWwindow* window;
 
+glm::vec3 points[8];
+
+glm::vec3 cube_points[8];
+
+int width = 1024;
+int height = 768;
+
+float particle_radius = 0.02;
+
+float horizontalAngle = 0.f;
+float verticalAngle = 0.f;
+
+float mouseSpeed = 1.f;
+float deltaTime = 0.000001f;
+
+GLuint vbo_spheres, vao_spheres_pos;
+GLuint vbo_cube, vao_cube_pos, vao_cube_indices;
+GLuint vs_sphere, fs_sphere, vs_basic, fs_basic;
+
+GLuint shader_program_spheres, shader_program_basic;
 
 /**********************************************************************
  *                           BASIC SHADERS                            *
  **********************************************************************/
 
-
-const char* vertex_shader =
+const char* vertex_shader_spheres =
 "#version 400\n"
 "uniform float pointScale;"
 "uniform float pointRadius;"
@@ -33,7 +52,7 @@ const char* vertex_shader =
 "  gl_Position = vec4(vp, 1.0)*MVP;"
 "}";
 
-const char* fragment_shader =
+const char* fragment_shader_spheres =
 "#version 400\n"
 "const float PI = 3.1415926535897932384626433832795;"
 "out vec4 frag_colour;"
@@ -53,41 +72,39 @@ const char* fragment_shader =
 "}"
 "}";
 
-/**********************************************************************
- *                WINDOW AND MOUSE/KEYBOARD PARAMETERS                *
- **********************************************************************/
+const char * vertex_shader_basic =
+"#version 400\n"
+"uniform mat4 MVP;"
+"in vec3 vp;"
+"void main(){"
+"	gl_Position = vec4(vp, 1)* MVP;"
+"}";
 
-glm::vec3 points[8];
-
-int width = 640;
-int height = 480;
-
-float particle_radius = 0.02;
-
-float horizontalAngle = 0.f;
-float verticalAngle = 0.f;
-
-float mouseSpeed = 1.f;
-float deltaTime = 0.000001f;
+const char * fragment_shader_basic = 
+"#version 400\n"
+"#out vec4 frag_colour;"
+"void main(){"
+"frag_colour = vec4(0,0,1,1);"
+"}";
 
 /**********************************************************************
- *                            MAIN PROGRAM                            *
+ *                       SOME HELPER FUNCTIONS                        *
  **********************************************************************/
 
-int main(void)
+void initWindow()
 {
 	if (!glfwInit())
 	{
 		fprintf(stderr, "ERROR: could not start GLFW3\n");
-		return 1;
+		exit(1);
 	} 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	GLFWwindow* window = glfwCreateWindow(width, height, "Hello Spheres", NULL, NULL);
+	window = glfwCreateWindow(width, height, "Hello Spheres", NULL, NULL);
 	if (!window)
 	{
 		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
 		glfwTerminate();
-		return 1;
+		exit(1);
 	}
 	glfwMakeContextCurrent(window);
 
@@ -100,88 +117,109 @@ int main(void)
 	const GLubyte* version = glGetString(GL_VERSION); // version as a string
 	printf("Renderer: %s\n", renderer);
 	printf("OpenGL version supported %s\n", version);
+}
 
-	// tell GL to only draw onto a pixel if the shape is closer to the viewer
+void glEnableCapabilities()
+{
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
 	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-
-	//face gauche
-	points[0] = glm::vec3(0,0,0);
-	points[1] = glm::vec3(0,1,0);
-	points[2] = glm::vec3(0.4,0,-1);
-	points[3] = glm::vec3(0.4,1,-1);
-
-	//face droite
-	points[4] = glm::vec3(1,0,0);
-	points[5] = glm::vec3(1,1,0);
-	points[6] = glm::vec3(1.4,0,-1);
-	points[7] = glm::vec3(1.4,1,-1);
-
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(glm::vec3), points, GL_STATIC_DRAW);
-
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
-	glCompileShader(vs);
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
-	glCompileShader(fs);
-
-	GLuint shader_programme = glCreateProgram();
-	glAttachShader(shader_programme, fs);
-	glAttachShader(shader_programme, vs);
-	glLinkProgram(shader_programme);
-
 	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 	glEnable(GL_POINT_SPRITE);
 	glEnable(GL_SMOOTH);
 	glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
-
 	glClearColor(1,1,1,1);
+}
 
-	glm::vec3 direction(0,0,1), right, up;
-	glm::vec3 position(0,0,3);
-	float diff_x = 0.f;
-	float diff_y = 0.f;
-	float sensitivity = 0.00001f;
+void  getNewVbo(GLuint *newVbo, unsigned int bufferSize, const GLvoid* data, GLenum usage)
+{
+	glGenBuffers(1, newVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, *newVbo);
+	glBufferData(GL_ARRAY_BUFFER, bufferSize, data, usage);
+}
+
+void getNewVao(GLuint *newVao, GLuint vbo, int attribNb)
+{
+	glGenVertexArrays(1, newVao);
+	glBindVertexArray(*newVao);
+	glEnableVertexAttribArray(attribNb);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+}
+
+void compileVertexAndFragmentShaders(GLuint *vs, GLuint *fs, const GLchar **string_vs, const GLchar **string_fs)
+{
+	*vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(*vs, 1, string_vs, NULL);
+	glCompileShader(*vs);
+	*fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(*fs, 1, string_fs, NULL);
+	glCompileShader(*fs);
+}
+
+void compileShaderProgram(GLuint *sp, GLuint vs, GLuint fs)
+{
+	*sp = glCreateProgram();
+	glAttachShader(*sp, fs);
+	glAttachShader(*sp, vs);
+	glLinkProgram(*sp);
+}
+
+/**********************************************************************
+ *                            MAIN PROGRAM                            *
+ **********************************************************************/
+
+int main(void)
+{
+	//call to helper functions
+	initWindow();
+	glEnableCapabilities();
+
+	//face gauche
+	points[0] = glm::vec3(-1,-1,0);
+	points[1] = glm::vec3(-1,1,0);
+	points[2] = glm::vec3(-1.4,-1,-1);
+	points[3] = glm::vec3(-1.4,1,-1);
+
+	//face droite
+	points[4] = glm::vec3(1,-1,0);
+	points[5] = glm::vec3(1,1,0);
+	points[6] = glm::vec3(1.4,-1,-1);
+	points[7] = glm::vec3(1.4,1,-1);
+
+	//opengl sphere buffers handling
+	getNewVbo(&vbo_spheres, 8 * sizeof(glm::vec3), points, GL_STATIC_DRAW);
+	getNewVao(&vao_spheres_pos, vbo_spheres, 0);
+
+	//shaders handling
+	compileVertexAndFragmentShaders(&vs_sphere, &fs_sphere, &vertex_shader_spheres, &fragment_shader_spheres);
+	compileShaderProgram(&shader_program_spheres, vs_sphere, fs_sphere);
 
 	while(!glfwWindowShouldClose(window))
 	{
-		static float i = 0.0001f;
-		// Reset mouse position for next frame
-
-		// wipe the drawing surface clear
+		//step 1 : clear screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shader_programme);
-		glBindVertexArray(vao);
 
+		//step 2 : handle mvp matrix
 		glm::mat4 m(1.f);
-		glm::mat4 v = glm::lookAt(position, glm::vec3(0,0,0), glm::vec3(0,1,0));
+		glm::mat4 v = glm::lookAt(glm::vec3(0,0,5), glm::vec3(0,0,0), glm::vec3(0,1,0));
 		glm::mat4 p = glm::perspective(45.f,(float)width/float(height), -1.0f, 100.f);
-
 		glm::mat4 mvp = p*v*m;
 
+		//step 3 : display spheres in associated shader program
+		glUseProgram(shader_program_spheres);
+		glBindVertexArray(vao_spheres_pos);
 
-		glUniform1f(glGetUniformLocation(shader_programme, "pointScale"), height / tanf(45.f*0.5f*(float)M_PI/180.0f));
-		glUniform1f(glGetUniformLocation(shader_programme, "pointRadius"), particle_radius);
-		glUniformMatrix4fv(glGetUniformLocation(shader_programme, "MVP"), 1, false, glm::value_ptr(mvp));
-
-		// draw points 0-3 from the currently bound VAO with current in-use shader
+		glUniform1f(glGetUniformLocation(shader_program_spheres, "pointScale"), height / tanf(45.f*0.5f*(float)M_PI/180.0f));
+		glUniform1f(glGetUniformLocation(shader_program_spheres, "pointRadius"), particle_radius);
+		glUniformMatrix4fv(glGetUniformLocation(shader_program_spheres, "MVP"), 1, false, glm::value_ptr(mvp));
 		glDrawArrays(GL_POINTS, 0, 8);
-		// update other events like input handling 
+
+		//step 4 : same thing for cube
+
+		//last step : read new events if some
 		glfwPollEvents();
-		// put the stuff we've been drawing onto the display
 		glfwSwapBuffers(window);
-		i+= 0.0001f;
 	}
 
 	glfwTerminate();
